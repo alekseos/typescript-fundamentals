994f0cd10124f473ca22223a0e16e5564ae0e7f8
Alekseev Andrey <alekseevandreygen@gmail.com>
2018-10-27 15:55:41 +0300

    New task

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -3,6 +3,19 @@
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+    let firstValue = 1;
+    let secondValue = 1;
+
+    yield firstValue;
+    yield secondValue;
+
+    while (true) {
+        let nextValue = firstValue + secondValue;
+
+        firstValue = secondValue;
+        secondValue = nextValue;
+
+        yield nextValue;
+    }
+}
